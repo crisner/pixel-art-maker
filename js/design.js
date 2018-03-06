b8050c400ee460664c6463a10b113a4a612e24d8
crisner <christie.reni@gmail.com>
2018-03-06 20:40:08 +0530

    added basic css, modified toolbox

--- js/design.js
+++ js/design.js
@@ -1,16 +1,28 @@
 let canvas = document.getElementById('art-board');
+let createArtBoard = document.getElementById('submit');
 let rows = document.getElementById('rows');
 let columns = document.getElementById('columns');
 let color = document.getElementById('color');
 let clear = document.getElementById('clear');
+
 let r = 0;
 let c = 0;
 
-rows.addEventListener('change', getRow);
-columns.addEventListener('change', getCol);
-// canvas.addEventListener('click', paint);
-canvas.addEventListener('dragover', paint, false);
+createArtBoard.addEventListener('click', getValues);
 clear.addEventListener('click', clearArtBoard);
+
+let action = null;
+canvas.addEventListener('mousedown', function() {
+    action = 'click';
+}, false);
+
+canvas.addEventListener('mousemove', paint, false);
+
+canvas.addEventListener('mouseup', function() {
+    if (action === 'drag' || action === 'click') {   
+        action = null;
+    }
+}, false);
 
 
 function clicked(e) {
@@ -18,11 +30,18 @@ function clicked(e) {
 }
 
 function paint(e) {
-    if (e.target.className === 'pixel') {
+    if (action === 'click') {
+        action = 'drag';
+    } 
+    if (e.target.className === 'pixel' && action === 'drag') {
         e.target.style.background = color.value;
-    } else {
-        console.log(false);
     }
+}
+
+function getValues() {
+    r = rows.valueAsNumber;
+    c = columns.valueAsNumber;
+    makeGrid();
 }
 
 function getRow(e) {
@@ -47,21 +66,24 @@ function makeGrid() {
     canvas.innerHTML = "";
     let eachRow = document.createElement('div');
     let pixel = document.createElement('span');
+    let size = Math.ceil(650/columns.valueAsNumber) - 2;
     
     for (let i = 0; i < r; i++) {
         row = document.createElement('div');
         canvas.appendChild(row);
-        row.style.height = '18px';
+        row.style.height = size +'px';
+        row.style.boxSizing = 'border-box';
         row.style.margin = '0 0 1px 0';
         for (let j = 0; j < c; j++) {
             pixel = document.createElement('span');
             row.appendChild(pixel);
+            row.style.boxSizing = 'border-box';
             pixel.style.display = 'inline-block';
             pixel.className = 'pixel';
-            pixel.style.height = '18px';
-            pixel.style.width = '18px';
+            pixel.style.height = size +'px';
+            pixel.style.width = size +'px';
             pixel.style.margin = '0 -1px 0 0';
-            pixel.style.border = '1px solid black';
+            pixel.style.border = '1px solid rgba(189, 189, 189,1.0)';
             pixel.style.background = 'transparent';
         }
     }
