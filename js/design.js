e99e2c87008d5637dccfb1fc07884eb3a8f8f3f6
crisner <christie.reni@gmail.com>
2018-03-17 00:54:37 +0530

    added fill canvas functionality, fixed bugs

--- js/design.js
+++ js/design.js
@@ -3,30 +3,94 @@ let createArtBoard = document.getElementById('submit');
 let rows = document.getElementById('rows');
 let columns = document.getElementById('columns');
 let color = document.getElementById('color');
+let brush = document.getElementById('brush');
 let clear = document.getElementById('clear');
+let eraser = document.getElementById('remove-color');
+let bucket = document.getElementById('fill');
 
 let r = 0;
 let c = 0;
+let action = null;
 
 createArtBoard.addEventListener('click', getValues);
 clear.addEventListener('click', clearArtBoard);
 
-let action = null;
-canvas.addEventListener('mousedown', function() {
-    action = 'click';
-}, false);
+bucket.addEventListener('click', function() {
+    canvas.addEventListener('click', fillArtBoard);
+});
 
-canvas.addEventListener('mousemove', paint, false);
+if (brush.checked) {
+    canvas.addEventListener('click', fill);
+    canvas.addEventListener('mousedown', function() {
+        action = 'click';
+    }, false);
 
-canvas.addEventListener('mouseup', function() {
-    if (action === 'drag' || action === 'click') {   
-        action = null;
-    }
-}, false);
+    canvas.addEventListener('mousemove', paint, false);
 
+    canvas.addEventListener('mouseup', function() {
+        if (action === 'drag' || action === 'click') {   
+            action = null;
+        }
+    }, false);
+}
+
+brush.addEventListener('click', function() {
+
+    canvas.addEventListener('click', fill);
+
+    canvas.addEventListener('mousedown', function() {
+        action = 'click';
+    }, false);
+
+    canvas.addEventListener('mousemove', function(e) {
+        if (brush.checked) {
+            paint(e);
+        }
+    }, false);
+
+    canvas.addEventListener('mouseup', function() {
+        if (action === 'drag' || action === 'click') {   
+            action = null;
+        }
+    }, false);
+        console.log("brush");
+});
+
+eraser.addEventListener('click', function() {
+    canvas.addEventListener('click', remove);
+
+    canvas.addEventListener('mousedown', function() {
+        action = 'click';
+    }, false);
+
+    canvas.addEventListener('mousemove', function(e) {
+        if (eraser.checked) {
+            removeColor(e);
+        }
+    }, false);
+
+    canvas.addEventListener('mouseup', function() {
+        if (action === 'drag' || action === 'click') {   
+            action = null;
+        }
+    }, false);
+    console.log("eraser");
+});
 
 function clicked(e) {
     console.log(e);
+}
+
+function fill(e) {
+    if (brush.checked && e.target.className === 'pixel' && action === null) {
+        e.target.style.background = color.value;
+    }
+}
+
+function remove(e) {
+    if (eraser.checked && e.target.className === 'pixel' && action === null) {
+        e.target.style.background = 'transparent';
+    }
 }
 
 function paint(e) {
@@ -36,6 +100,25 @@ function paint(e) {
     if (e.target.className === 'pixel' && action === 'drag') {
         e.target.style.background = color.value;
     }
+}
+
+function removeColor(e) {
+    if (action === 'click') {
+        action = 'drag';
+    } 
+    if (e.target.className === 'pixel' && action === 'drag') {
+        e.target.style.background = 'transparent';
+    }
+}
+
+function fillArtBoard() {
+    let pixelClass = document.getElementsByClassName('pixel');
+    let length = pixelClass.length;
+    if (bucket.checked && action === null) {
+        for (let i = 0; i < length; i++) {
+            pixelClass[i].style.background = color.value;
+        }
+    }  
 }
 
 function getValues() {
